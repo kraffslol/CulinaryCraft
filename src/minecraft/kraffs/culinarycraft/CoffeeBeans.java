be69837df73cc1b413676fa30d53ed60e27f208f
Rickard Ekman <xm0rfin@gmail.com>
2013-03-20 03:19:05 +0100

    asd

--- src/minecraft/kraffs/culinarycraft/CoffeeBeans.java
+++ src/minecraft/kraffs/culinarycraft/CoffeeBeans.java
@@ -8,10 +8,8 @@ public class CoffeeBeans extends ItemSeeds {
 
 	public CoffeeBeans(int par1, int par2, int par3) {
 		super(par1, par2, par3);
-		//setUnlocalizedName("CoffeeBeans");
 		setMaxStackSize(64);
 		setCreativeTab(CreativeTabs.tabMisc);
-		// TODO Auto-generated constructor stub
 	}
 	
 	@Override
