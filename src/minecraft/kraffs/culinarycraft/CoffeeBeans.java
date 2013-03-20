6ea6caec26da3c54258a56d0e574aa0f938bc8a8
Rickard Ekman <xm0rfin@gmail.com>
2013-03-20 03:15:23 +0100

    Beans

--- src/minecraft/kraffs/culinarycraft/CoffeeBeans.java
+++ src/minecraft/kraffs/culinarycraft/CoffeeBeans.java
@@ -1,6 +1,7 @@
 package kraffs.culinarycraft;
 
 import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.ItemSeeds;
 
 public class CoffeeBeans extends ItemSeeds {
@@ -8,6 +9,8 @@ public class CoffeeBeans extends ItemSeeds {
 	public CoffeeBeans(int par1, int par2, int par3) {
 		super(par1, par2, par3);
 		//setUnlocalizedName("CoffeeBeans");
+		setMaxStackSize(64);
+		setCreativeTab(CreativeTabs.tabMisc);
 		// TODO Auto-generated constructor stub
 	}
 	
